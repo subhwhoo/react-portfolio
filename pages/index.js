dbaab8412168f5a363ce24e6d3e27cc9b5598cbc
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-02-13 16:05:18 +0530

    Centered Image

--- pages/index.js
+++ pages/index.js
@@ -77,23 +77,30 @@ export default function Home() {
               consumer use.
             </p>
             <p className={"text-md py-2 leading-8 text-gray-800 "}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad,
-              beatae eligend.
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
             </p>
           </div>
           <div>
-            <div>
-              <Image src={design} width={100} height={100} />
-              <h3 className={"text-lg font-medium pt-8 pb-2"}>
+            <div className="text-center shadow-xl p-10 rounded-xl my-10  dark:bg-white flex-1">
+              <Image
+                src={design}
+                width={100}
+                height={100}
+                className={"m-auto"}
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2  ">
                 Beautiful Designs
               </h3>
-              <p className={"py-2"}>
-                Creating elegant designs suited for your needs design theory.
+              <p className="py-2">
+                Creating elegant designs suited for your needs following core
+                design theory.
               </p>
-              <h4 className={"py-4 text-teal-600"}>Design tools i use</h4>
-              <p className={"text-gray-800 py-1 "}>Figma</p>
-              <p className={"text-gray-800 py-1 "}>Photoshop</p>
-              <p className={"text-gray-800 py-1 "}>Illustrator</p>
+              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
             </div>
           </div>
         </section>
